6b2165db6c3e584240d8aea5812d1dbce8548bc1
Prgckwb <ckwb.uec@gmail.com>
2022-05-20 15:37:19 +0900

    add: comment

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import argparse
 import webvtt
 from yt_dlp import YoutubeDL
 
+# Setup to extract only subtitles without downloading the video itself.
 YDL_OPTIONS = {
     "skip_download": True,
     "writeautomaticsub": True,
 }
 
 
+# Set command line arguments
 def init_argument():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="Youtube video link to download subtitles")
@@ -16,6 +18,7 @@ def init_argument():
     return argument
 
 
+# Download subtitle files and obtain meta information.
 def get_video_info(url):
     with YoutubeDL(YDL_OPTIONS) as ydl:
         info = ydl.extract_info(url)
@@ -23,21 +26,21 @@ def get_video_info(url):
     return info
 
 
+# Extract only the text from the subtitle vvt file.
 def get_caption(video_info):
     subtitle_file_name = video_info["requested_subtitles"]["en"]["filepath"]
     vtt = webvtt.read(subtitle_file_name)
-    captions = [caption.text for caption in vtt]
+    captions_duplicated = [caption.text for caption in vtt]
 
-    new_captions = []
+    captions = []
 
-    for caption in captions:
+    for caption in captions_duplicated:
         texts = caption.split('\n')
 
         for text in texts:
-            new_captions.append(text)
+            captions.append(text)
 
-    # 重複削除
-    captions = list(dict.fromkeys(new_captions))
+    captions = list(dict.fromkeys(captions))
 
     all_text = " ".join(captions)
     return all_text
@@ -51,5 +54,6 @@ if __name__ == '__main__':
     subtitles = get_caption(video_info)
 
     video_title = video_info["title"]
+
     with open(f"{video_title}_sub.txt", "w") as f:
         f.write(subtitles)
